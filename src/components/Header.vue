c7278a63a937d578ed67d04907e97e5d92255672
jhonatanmizu <natanjesuss20@gmail.com>
2022-06-28 12:51:10 -0300

    Added Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  {{counter}}
+  <button @click="this.counter++">Increment</button>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+    name:"Header",
+    setup(){
+        const counter = ref(0)
+        return {
+            counter
+        }
+    },
+    mounted() {
+      alert("Funcionando")  
+    },
+}
+</script>
+
+<style>
+
+</style>
